18b3218369ef1e7642b3175baef4179ce1b48e7c
malinjr07 <malinjr07@gmail.com>
2022-02-20 17:16:34 +0600

    products done

--- Front-End/src/pages/Products.jsx
+++ Front-End/src/pages/Products.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+// import { useEffect, useState } from 'react/cjs/react.production.min';
+import { useEffect, useState } from 'react/cjs/react.development';
 import Breadcrumb from '../components/Breadcrumb';
+import { topSellingData } from '../components/core/core';
 import FilterBar from '../components/FilterBar';
+import Pagination from '../components/Pagination';
+import SingleItem from '../components/SingleItem';
 
 function Products() {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    setProduct(topSellingData);
+    return () => {
+      setProduct([]);
+    };
+  }, []);
   return (
     <>
       <Breadcrumb />
@@ -10,7 +22,43 @@ function Products() {
         <div className='flex-wrap w-[31%] pr-6 '>
           <FilterBar />
         </div>
-        <div className='flex-wrap w-[69%] pr-6 '>{/* <FilterBar /> */}</div>
+        <div className='flex-wrap w-[69%] pr-6 '>
+          <div className='flex flex-col bg-white'>
+            <div className='py-12 px-16 flex justify-start items-center '>
+              <h5 className='font-semibold leading-[120%] '>
+                Fashion Accessories
+              </h5>
+            </div>
+            <div className='py-12 px-16 flex justify-start items-center '>
+              <button className='py-8 px-13 text-md capitalize font-semibold'>
+                {' '}
+                Selling <i class='fa-solid fa-bolt'></i>
+              </button>
+              <button className='py-8 px-13 text-md capitalize font-semibold'>
+                {' '}
+                New Products
+              </button>
+              <button className='py-8 px-13 text-md capitalize font-semibold'>
+                {' '}
+                Price{' '}
+                <i class=' rotate-90 fa-solid fa-arrow-right-arrow-left'></i>
+              </button>
+            </div>
+            <div className='flex flex-row py-8 px-16 flex-wrap '>
+              {product.map((item, index) => (
+                <div className={`w-1/4 py-8 px-8`}>
+                  <SingleItem
+                    key={index}
+                    content={item}
+                    size='180px'
+                    showDiscount={true}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+          <Pagination />
+        </div>
       </section>
     </>
   );
