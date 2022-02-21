08f63f502f47baf05b0cb441211d8eddc635cd1a
malinjr07 <malinjr07@gmail.com>
2022-02-21 13:45:53 +0600

    checkout item done

---
+++ Front-End/src/components/CheckoutItem.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import url from './core/core';
+
+function CheckoutItem() {
+  return (
+    <>
+      <div className='mb-20 flex flex-row justify-between'>
+        <div className='flex flex-row'>
+          <div className='w-[100px] h-[100px] rounded overflow-hidden mr-16 '>
+            <img
+              src={`${url}/img/item.jpg`}
+              alt=''
+              className='w-full h-full '
+            />
+          </div>
+          <div className='flex flex-col w-[265px] justify-start '>
+            <h3 className='leading-[150%] font-normal '>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur, rerum?{' '}
+            </h3>
+          </div>
+        </div>
+      </div>
+      <div className='flex justify-between'>
+        <h5 className='leading-[140%] font-semibold '>Transport Fee</h5>
+        <h5 className='leading-[140%] font-semibold '>60$</h5>
+      </div>
+      <div className='flex justify-between'>
+        <h5 className='leading-[140%] font-semibold '>Unit Price</h5>
+        <h5 className='leading-[140%] font-semibold '>90$</h5>
+      </div>
+      <div className='flex justify-start my-20'>
+        <button className='w-[30px] h-[30px] border border-ash active:bg-slate-300 rounded-tl rounded-bl '>
+          -
+        </button>
+        <input type='text' value='1' className='w-[45px] border-ash h-[30px]' />
+        <button className='w-[30px] h-[30px] border border-ash active:bg-slate-300 rounded-tr rounded-br '>
+          +
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default CheckoutItem;
--- Front-End/src/pages/Checkout.jsx
+++ Front-End/src/pages/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Breadcrumb from '../components/Breadcrumb';
+import CheckoutItem from '../components/CheckoutItem';
 import url from '../components/core/core';
 
 function Checkout() {
@@ -8,7 +9,7 @@ function Checkout() {
       <Breadcrumb />
       <section className='container pb-16 flex flex-row'>
         <div className='flex-wrap w-[50%] pr-6 '>
-          <div className='flex flex-col w-full h-[942px] p-[24px] bg-white overflow-y-scroll '>
+          <div className='flex flex-col w-full h-[950px] p-[24px] bg-white overflow-y-scroll '>
             <div className='mb-20 flex justify-start items-center '>
               <div className='w-[35px] h-[35px] rounded-[50%] mr-16 overflow-hidden'>
                 <img
@@ -17,9 +18,10 @@ function Checkout() {
                   className='w-full h-full'
                 />
               </div>
-              <h2 className='leading-[150%] font-semibold'>McDonalds-Icon</h2>
+              <h2 className='leading-[150%] font-semibold'>McDonalds</h2>
             </div>
-            <div className='mb-20 flex flex-row '></div>
+            <CheckoutItem />
+            <CheckoutItem />
           </div>
         </div>
       </section>
