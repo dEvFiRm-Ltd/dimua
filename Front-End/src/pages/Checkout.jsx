d4b6c45f3afd83e7a078eca92dc6077389022ec7
malinjr07 <malinjr07@gmail.com>
2022-02-21 14:45:36 +0600

    blank checkout

--- Front-End/src/pages/Checkout.jsx
+++ Front-End/src/pages/Checkout.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
+import BlankCheckout from '../components/BlankCheckout';
 import Breadcrumb from '../components/Breadcrumb';
-import CheckoutItem from '../components/CheckoutItem';
-import url from '../components/core/core';
+// import ProductCheckout from '../components/ProductCheckout';
 
 function Checkout() {
   return (
     <>
       <Breadcrumb />
-      <section className='container pb-16 flex flex-row'>
-        <div className='flex-wrap w-[50%] pr-6 '>
-          <div className='flex flex-col w-full h-[950px] p-[24px] bg-white overflow-y-scroll '>
-            <div className='mb-20 flex justify-start items-center '>
-              <div className='w-[35px] h-[35px] rounded-[50%] mr-16 overflow-hidden'>
-                <img
-                  src={`${url}/img/McDonalds-Icon.png`}
-                  alt=''
-                  className='w-full h-full'
-                />
-              </div>
-              <h2 className='leading-[150%] font-semibold'>McDonalds</h2>
-            </div>
-            <CheckoutItem />
-            <CheckoutItem />
-          </div>
-        </div>
+      <section className='container pb-16 flex flex-row justify-center'>
+        {/* <ProductCheckout /> */}
+        <BlankCheckout />
       </section>
     </>
   );
