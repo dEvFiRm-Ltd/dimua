8adbcf69a2975984f49e8e500ee3b880b8a17ed7
malinjr07 <malinjr07@gmail.com>
2022-02-24 15:46:53 +0600

    functional02

--- Assets/react/src/components/Loader.js
+++ Assets/react/src/components/Loader.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 /**
  * Renders the preloader
  */
-class PreLoaderWidget extends Component {
-
-    render() {
-        return (
-            <div className="preloader">
-                <div className="status">
-                    <div className="spinner-border avatar-sm text-primary m-2" role="status"></div>
-                </div>
-            </div>
-        )
-    }
+function PreLoaderWidget() {
+  return (
+    <div className='preloader'>
+      <div className='status'>
+        <div
+          className='spinner-border avatar-sm text-primary m-2'
+          role='status'
+        ></div>
+      </div>
+    </div>
+  );
 }
 
 export default PreLoaderWidget;
--- Assets/react/src/components/RichTextEditor.js
+++ Assets/react/src/components/RichTextEditor.js
@@ -1,57 +1,45 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { EditorState, convertToRaw, ContentState } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg'
+import { Editor } from 'react-draft-wysiwyg';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-class RichTextEditor extends Component {
-    constructor(props) {
-        super(props);
+const RichTextEditor = (props) => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
-        this.state = {
-            editorState: EditorState.createEmpty(),
-        };
-
-        this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  useEffect(() => {
+    if (props.initialContent) {
+      const { contentBlocks, entityMap } = htmlToDraft(props.initialContent);
+      const contentState = ContentState.createFromBlockArray(
+        contentBlocks,
+        entityMap
+      );
+      setEditorState({
+        editorState: EditorState.createWithContent(contentState),
+      });
     }
+  }, [props.initialContent]);
 
-    componentDidMount(prevProps) {
-        if (this.props.initialContent) {
-            const { contentBlocks, entityMap } = htmlToDraft(this.props.initialContent);
-            const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-            this.setState({ editorState: EditorState.createWithContent(contentState) });
-        }
-    }
-
-    /**
-     * On editor body change
-     */
-    onEditorStateChange = (editorState) => {
-        this.setState({
-            editorState,
-        });
-        const body = draftToHtml(convertToRaw(editorState.getCurrentContent()));
-        this.props.onEditorContentChange && this.props.onEditorContentChange(body);
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-
-                <Editor
-                    editorState={this.state.editorState}
-                    wrapperClassName="rich-editor-wrapper"
-                    editorClassName="rich-editor"
-                    onEditorStateChange={this.onEditorStateChange}
-                    toolbarClassName={this.props.hideToolbar ? 'hide-toolbar' : ''}
-                />
-
-            </React.Fragment>
-        )
-    }
-}
-
+  const onEditorStateChange = (editorState) => {
+    setEditorState({
+      editorState,
+    });
+    const body = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+    props.onEditorContentChange && props.onEditorContentChange(body);
+  };
+  return (
+    <React.Fragment>
+      <Editor
+        editorState={editorState}
+        wrapperClassName='rich-editor-wrapper'
+        editorClassName='rich-editor'
+        onEditorStateChange={onEditorStateChange}
+        toolbarClassName={props.hideToolbar ? 'hide-toolbar' : ''}
+      />
+    </React.Fragment>
+  );
+};
 
 export default RichTextEditor;
