6e42ec0293608b58f5f19a7f80542bc47dd6e908
malinjr07 <malinjr07@gmail.com>
2022-02-24 16:48:29 +0600

    themecustomizer left

--- Assets/react/src/components/RightSidebar.js
+++ Assets/react/src/components/RightSidebar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -6,79 +6,64 @@ import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import { XCircle } from 'react-feather';
 
-class RightSideBar extends Component {
-    rightBarNodeRef;
+const RightSideBar = ({ title = 'Right Sidebar' }, props) => {
+  let rightBarNodeRef = useRef(null);
+  const component = props.children || null;
 
-    static defaultProps = {
-        title: 'Right Sidebar'
-    }
+  /**
+   * Handles the close
+   */
+  const handleClose = (e) => {
+    e.preventDefault();
+    hide();
+  };
 
-    constructor(props) {
-        super(props);
-        this.handleClose = this.handleClose.bind(this);
-        this.state = {}
-    }
+  /**
+   * Hide rightside bar
+   */
+  function hide() {
+    document.body.classList.remove('right-bar-enabled');
+  }
+  useEffect(() => {
+    document.addEventListener(
+      'mousedown',
+      (e) => {
+        if (rightBarNodeRef.contains(e.target)) return;
+        hide();
+      },
+      false
+    );
 
-    /**
-     * 
-     */
-    componentDidMount = () => {
-        document.addEventListener('mousedown', this.handleOtherClick, false);
-    }
+    return () => {
+      document.removeEventListener(
+        'mousedown',
+        (e) => {
+          if (rightBarNodeRef.contains(e.target)) return;
+          hide();
+        },
+        false
+      );
+    };
+  }, []);
 
-    /**
-     * 
-     */
-    componentWillUnmount = () => {
-        document.removeEventListener('mousedown', this.handleOtherClick, false);
-    }
-    /**
-     * Handles the close
-     */
-    handleClose = (e) => {
-        e.preventDefault();
-        this.hide();
-    }
-
-    /**
-     * Handle the click anywhere in doc
-     */
-    handleOtherClick = (e) => {
-        if (this.rightBarNodeRef.contains(e.target))
-            return;
-        // else hide the right sidebar
-        this.hide();
-    }
-
-    /**
-     * Hide rightside bar
-     */
-    hide() {
-        document.body.classList.remove("right-bar-enabled");
-    }
-
-    render() {
-
-        const title = this.props.title;
-        const component = this.props.children || null;
-
-        return (
-            <React.Fragment>
-                <div className="right-bar" ref={node => this.rightBarNodeRef = node}>
-                    <div className="rightbar-title">
-                        <Link to="#" className="right-bar-toggle float-right" onClick={this.handleClose}>
-                            <XCircle />
-                        </Link>
-                        <h5 className="m-0">{title}</h5>
-                    </div>
-                    <PerfectScrollbar>
-                        {component}
-                    </PerfectScrollbar>
-                </div>
-                <div className="rightbar-overlay"></div>
-            </React.Fragment>
-        )
-    }
-}
+  return (
+    <React.Fragment>
+      <div className='right-bar' ref={(node) => (rightBarNodeRef = node)}>
+        <div className='rightbar-title'>
+          <Link
+            to='#'
+            className='right-bar-toggle float-right'
+            onClick={handleClose}
+          >
+            <XCircle />
+          </Link>
+          <h5 className='m-0'>{title}</h5>
+        </div>
+        <PerfectScrollbar>{component}</PerfectScrollbar>
+      </div>
+      <div className='rightbar-overlay'></div>
+    </React.Fragment>
+  );
+};
 
 export default connect()(RightSideBar);
