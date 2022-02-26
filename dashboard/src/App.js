aa88bb53a25edc3924ed25070458ee8da8c9e895
malinjr07 <malinjr07@gmail.com>
2022-02-26 11:23:24 +0600

    dashboard

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,15 +1,40 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import Dashboard from './Pages/Dashboard';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    black: {
+      main: '#0D0D0D',
+    },
+    white: {
+      primary: '#F2F2F2',
+    },
+    cream: {
+      main: '#736555',
+    },
+    green: {
+      main: '#5FD98A',
+      dark: '#5EBF80',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className='App'>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path='/' exact element={<Navigate to='dashboard' />} />
-        <Route path='dashboard' element={<h1>Home </h1>}>
-          <Route path='home' element={<h1>Dashboard</h1>} />
+        <Route path='dashboard' element={<Dashboard />}>
+          <Route index element={<Home />} />
         </Route>
+        <Route path='login' element={<SignIn />} />
+        <Route path='register' element={<SignUp />} />
       </Routes>
-    </div>
+    </ThemeProvider>
   );
 }
 
