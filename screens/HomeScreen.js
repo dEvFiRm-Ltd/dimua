a534cf113a7cd325e85f9e087ad9ec51806224ec
malinjr07 <malinjr07@gmail.com>
2022-04-05 12:25:20 +0600

    done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,49 +1,83 @@
 import * as React from 'react';
 import { Button, View } from 'react-native';
 import { color } from '../assets/css/style';
-import { useDrawerProgress } from '@react-navigation/drawer';
+import { useDrawerStatus } from '@react-navigation/drawer';
 import Animated, {
   Extrapolate,
   interpolate,
-  interpolateNode,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 
 export default function HomeScreen({ navigation }) {
-  const progress = useDrawerProgress();
-  const scale = interpolateNode(progress.value, {
-    inputRange: [0, 1],
-    outputRange: [1, 0.8],
-    extrapolate: Extrapolate.CLAMP,
+  const scaleAnimation = useSharedValue(0);
+  const isDrawerOpen = useDrawerStatus();
+  const animatedParentStyle = useAnimatedStyle(() => {
+    const scale = interpolate(scaleAnimation.value, [0, 1], [1, 0.75], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    const translateX = interpolate(scaleAnimation.value, [0, 1], [0, -40], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    const translateY = interpolate(scaleAnimation.value, [0, 1], [0, 40], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    const borderRadius = interpolate(scaleAnimation.value, [0, 1], [0, 30], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    return {
+      transform: [{ scale }, { translateX }, { translateY }],
+      borderRadius,
+    };
   });
-  const borderRadius = interpolateNode(progress.value, {
-    inputRange: [0, 1],
-    outputRange: [1, 20],
-    extrapolate: Extrapolate.CLAMP,
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(scaleAnimation.value, [0, 1], [0, 40], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    const translateY = interpolate(scaleAnimation.value, [0, 1], [0, -50], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    const borderRadius = interpolate(scaleAnimation.value, [0, 1], [0, 30], {
+      extrapolateRight: Extrapolate.CLAMP,
+    });
+    return {
+      transform: [{ translateX }, { translateY }],
+      borderRadius,
+    };
   });
-  const animatedStyle = {
-    borderRadius,
-    transform: [{ scale }],
-  };
+  React.useEffect(() => {
+    if (isDrawerOpen === 'open') {
+      scaleAnimation.value = withTiming(1, { duration: 500 });
+    } else {
+      scaleAnimation.value = withTiming(0, { duration: 500 });
+    }
+  }, [isDrawerOpen]);
 
   return (
     <Animated.View
       style={[
         {
+          backgroundColor: 'rgba(242, 242, 242, 0.2)',
           flex: 1,
-          elevation: 10,
-          overflow: 'hidden',
-          backgroundColor: color.base,
+          elevation: 0,
         },
-        animatedStyle,
+        animatedParentStyle,
       ]}
     >
-      <Button onPress={() => navigation.toggleDrawer()} title='aaslem' />
+      <Animated.View
+        style={[
+          {
+            flex: 1,
+            elevation: 0,
+            overflow: 'hidden',
+            backgroundColor: color.base,
+          },
+          animatedStyle,
+        ]}
+      >
+        <Button onPress={() => navigation.toggleDrawer()} title='alem' />
+      </Animated.View>
     </Animated.View>
-    // <View style={{ flex: 1, backgroundColor: color.base }}>
-    //   <Button onPress={() => navigation.toggleDrawer()} title='alem' />
-    // </View>
   );
 }
