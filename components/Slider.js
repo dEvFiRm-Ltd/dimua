cc8e157ec721a244c548d7aa4620cf69478ae907
malinjr07 <malinjr07@gmail.com>
2022-04-07 09:49:51 +0600

    slider done

--- components/Slider.js
+++ components/Slider.js
@@ -1,69 +1,89 @@
-import { View, Image, FlatList } from 'react-native';
-import React, { useRef, useState } from 'react';
-import style, { color } from '../assets/css/style';
-const Slider = ({ data }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const flatlistRef = useRef(null);
-  // const { width, height } = Dimensions.get('window');
-  const onViewRef = useRef(({ changed }) => {
-    if (changed[0].isViewable) {
-      setCurrentIndex(changed[0].index);
-    }
-  });
-  return (
-    <>
-      <FlatList
-        style={{ flexGrow: 0 }}
-        horizontal
-        pagingEnabled
-        keyExtractor={(item, index) => index.toString()}
-        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 95 }}
-        ref={(ref) => {
-          flatlistRef.current = ref;
+import React, { useRef, useState, useEffect } from 'react';
+import Carousel, {
+  Pagination,
+  ParallaxImage,
+} from 'react-native-snap-carousel';
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Image,
+} from 'react-native';
+import { products } from '../mocks/data';
+import { color } from '../assets/css/style';
+
+const { width: screenWidth } = Dimensions.get('window');
+
+const Slider = (props) => {
+  const [entries, setEntries] = useState([]);
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    setEntries(products);
+  }, []);
+
+  const renderItem = ({ item }, parallaxProps) => {
+    return (
+      <View
+        style={{
+          width: 241,
+          height: 241,
+          borderRadius: 241 / 2,
+          overflow: 'hidden',
+          shadowColor: color.black,
+          shadowOffset: { width: 0, height: 1 },
+          shadowRadius: 2,
+          elevation: 15,
         }}
-        showsHorizontalScrollIndicator={false}
-        onViewableItemsChanged={onViewRef.current}
-        data={data}
-        renderItem={({ item, index, separators }) => {
-          const imgUrl = { uri: item.image };
-          return (
-            <View
-              key={index}
-              style={{
-                width: 241,
-                height: 241,
-                margin: 30,
-                borderRadius: 241 / 2,
-                overflow: 'hidden',
-                shadowColor: color.black,
-                shadowOffset: { width: 0, height: 1 },
-                shadowRadius: 2,
-                elevation: 15,
-              }}
-            >
-              <Image
-                source={imgUrl}
-                resizeMode='stretch'
-                style={{ width: null, height: null, flex: 1 }}
-              />
-            </View>
-          );
-        }}
-      />
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        {data.map((item, index) => (
-          <View
-            key={index}
-            style={{
-              width: 15,
-              height: 15,
-              borderRadius: 15 / 2,
-              backgroundColor: index === currentIndex ? color.red : color.ash,
-            }}
-          />
-        ))}
+      >
+        <ParallaxImage
+          source={{ uri: item.image }}
+          containerStyle={{
+            width: null,
+            height: null,
+            flex: 1,
+          }}
+          style={{
+            resizeMode: 'contain',
+          }}
+          parallaxFactor={0.4}
+          {...parallaxProps}
+        />
       </View>
-    </>
+    );
+  };
+
+  return (
+    <View>
+      <Carousel
+        sliderWidth={screenWidth}
+        sliderHeight={screenWidth}
+        itemWidth={screenWidth - 60}
+        data={entries}
+        onSnapToItem={(index) => setActiveSlide(index)}
+        renderItem={renderItem}
+        hasParallaxImages={true}
+      />
+      <Pagination
+        dotsLength={entries.length}
+        activeDotIndex={activeSlide}
+        containerStyle={{ backgroundColor: 'transparent' }}
+        dotStyle={{
+          width: 8,
+          height: 8,
+          borderRadius: 4,
+          marginHorizontal: 6,
+          backgroundColor: color.red,
+        }}
+        inactiveDotStyle={{
+          backgroundColor: color.ash,
+        }}
+        inactiveDotScale={1}
+      />
+    </View>
   );
 };
 
---
+++ components/Slider.old.js
@@ -0,0 +1,70 @@
+import { View, Image, FlatList } from 'react-native';
+import React, { useRef, useState } from 'react';
+import style, { color } from '../assets/css/style';
+const Slider = ({ data }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const flatlistRef = useRef(null);
+  // const { width, height } = Dimensions.get('window');
+  const onViewRef = useRef(({ changed }) => {
+    if (changed[0].isViewable) {
+      setCurrentIndex(changed[0].index);
+    }
+  });
+  return (
+    <>
+      <FlatList
+        style={{ flexGrow: 0 }}
+        horizontal
+        pagingEnabled
+        keyExtractor={(item, index) => index.toString()}
+        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 95 }}
+        ref={(ref) => {
+          flatlistRef.current = ref;
+        }}
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewRef.current}
+        data={data}
+        renderItem={({ item, index, separators }) => {
+          const imgUrl = { uri: item.image };
+          return (
+            <View
+              key={index}
+              style={{
+                width: 241,
+                height: 241,
+                margin: 30,
+                borderRadius: 241 / 2,
+                overflow: 'hidden',
+                shadowColor: color.black,
+                shadowOffset: { width: 0, height: 1 },
+                shadowRadius: 2,
+                elevation: 15,
+              }}
+            >
+              <Image
+                source={imgUrl}
+                resizeMode='stretch'
+                style={{ width: null, height: null, flex: 1 }}
+              />
+            </View>
+          );
+        }}
+      />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        {data.map((item, index) => (
+          <View
+            key={index}
+            style={{
+              width: 8,
+              height: 8,
+              borderRadius: 8 / 2,
+              backgroundColor: index === currentIndex ? color.red : color.ash,
+            }}
+          />
+        ))}
+      </View>
+    </>
+  );
+};
+
+export default Slider;
